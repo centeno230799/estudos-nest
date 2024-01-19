fa2a828a6103a70909d04a25e9e2ac46f1ba413b
centeno230799 <gabriel@arcotech.io>
2024-01-18 23:27:39 -0300

    chore(*): anotacoes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// module -> decorator adicionando recursos em exec time dentro da classe vazia
 @Module({
   imports: [],
   controllers: [AppController], // Todos os requests são interceptados por controles que definem uma rota
