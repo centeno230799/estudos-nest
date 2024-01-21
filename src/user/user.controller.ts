7399b0b674296dcfb0f5be3acbb7a60356d02dc6
centeno230799 <gabriel@arcotech.io>
2024-01-21 20:18:56 -0300

    feat(*): read

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
   @Post() // decorator sempre vem antes do que quero decorar
-
   // acessa o body da request e atribui a variável
   async create(@Body() body) {
     return { body };
+  }
+
+  @Get()
+  async list() {
+    return { users: [] };
+  }
+
+  @Get(':id')
+  async show(@Param() params) {
+    return { user: {}, params };
   }
 }
