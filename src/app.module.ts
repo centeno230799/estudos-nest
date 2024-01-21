6892c987c095918b2317188ee00efe95030ef370
centeno230799 <gabriel@arcotech.io>
2024-01-21 20:11:24 -0300

    feat(*): criado metodo de post para criacao de usuarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 // module -> decorator adicionando recursos em exec time dentro da classe vazia
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController], // Todos os requests são interceptados por controles que definem uma rota
   providers: [AppService],
 })
