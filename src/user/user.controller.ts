3ba8b839d2a12c2ab69aa55ffc73ea0568f62357
centeno230799 <gabriel@arcotech.io>
2024-01-21 20:50:34 -0300

    feat(*): create user DTO

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Controller('users')
 export class UserController {
   @Post() // decorator sempre vem antes do que quero decorar
   // acessa o body da request e atribui a variável
-  async create(@Body() body) {
+  async create(@Body() body: CreateUserDTO) {
     return { body };
   }
 
