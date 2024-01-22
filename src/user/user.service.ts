4dab718dcf5437d69e038597049f6a72d453d4c7
centeno230799 <gabriel@arcotech.io>
2024-01-21 22:26:15 -0300

    feat(*): integrando metodos com db

---
+++ src/user/user.service.ts
@@ -0,0 +1,96 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdatePutUserDTO } from './dto/update-put-user.dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: CreateUserDTO) {
+    // no retorno não tem necessidade do await
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async list() {
+    return this.prisma.user.findMany();
+  }
+
+  async show(id: number) {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(
+    id: number,
+    { name, email, password, birthAt }: UpdatePutUserDTO,
+  ) {
+    await this.exists(id);
+
+    return this.prisma.user.update({
+      data: {
+        name,
+        email,
+        password,
+        birthAt: birthAt ? new Date(birthAt) : null,
+      },
+      where: {
+        id,
+      },
+    });
+  }
+
+  async updatePartial(
+    id: number,
+    { name, email, password, birthAt }: UpdatePatchUserDTO,
+  ) {
+    await this.exists(id);
+
+    const data: any = {};
+
+    if (birthAt) {
+      data.birthAt = new Date(birthAt);
+    }
+
+    if (email) {
+      data.email = email;
+    }
+
+    if (name) {
+      data.name = name;
+    }
+
+    if (password) {
+      data.password = password;
+    }
+
+    return this.prisma.user.update({
+      data,
+      where: {
+        id,
+      },
+    });
+  }
+
+  async delete(id: number) {
+    await this.exists(id);
+
+    return this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async exists(id: number) {
+    if (!(await this.show(id))) {
+      throw new NotFoundException(`O usuário ${id} não existe`);
+    }
+  }
+}
