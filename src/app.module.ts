27d8dbd3183cc1c451c8b684644478456df6c4cf
centeno230799 <gabriel@arcotech.io>
2024-01-22 22:57:26 -0300

    feat(*): add throttle para evitar ddos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
+import { APP_GUARD } from '@nestjs/core';
 import { Module, forwardRef } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 // module -> decorator adicionando recursos em exec time dentro da classe vazia
 @Module({
-  imports: [forwardRef(() => UserModule), forwardRef(() => AuthModule)],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+    forwardRef(() => UserModule),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [AppController], // Todos os requests são interceptados por controles que definem uma rota
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   exports: [AppService],
 })
 export class AppModule {}
